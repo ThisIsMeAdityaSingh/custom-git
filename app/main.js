395fdc777e4b48c42c861893845313d76caf75c8
ThisIsMeAdityaSingh <thisismeadityasingh99@gmail.com>
2024-10-17 21:08:17 +0530

    Custom create git repo function

--- app/main.js
+++ app/main.js
@@ -4,17 +4,22 @@ const path = require("path");
 const command = process.argv[2];
 
 function createGitDirectory() {
-  fs.mkdirSync(path.join(process.cwd(), ".git"), { recursive: true });
-  fs.mkdirSync(path.join(process.cwd(), ".git", "objects"), {
-    recursive: true,
-  });
-  fs.mkdirSync(path.join(process.cwd(), ".git", "refs"), { recursive: true });
+    // this creates a .git directory.
+    fs.mkdirSync(path.join(process.cwd(), ".git"), {recursive: true});
 
-  fs.writeFileSync(
-    path.join(process.cwd(), ".git", "HEAD"),
-    "ref: refs/heads/main\n"
-  );
-  console.log("Initialized git directory");
+    // this would create an object directory inside .git directory
+    fs.mkdirSync(path.join(process.cwd(), ".git", "objects"), {recursive: true});
+
+    // this would create an ref directory inside .git directory
+    fs.mkdirSync(path.join(process.cwd(), ".git", "refs"), {recursive: true});
+
+    // this would create an HEAD directory inside .git directory and write something in it
+    fs.writeFileSync(
+        path.join(process.cwd(), ".git", "HEAD"),
+        "ref: refs/heads/main\n"
+    );
+
+    console.log("Initialized git directory");
 }
 
 switch (command) {
